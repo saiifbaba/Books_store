e97394845fbbd4256c3bdf4298d181b1a50cd8e0
saiifbaba <saifbaba888@gmail.com>
2024-05-18 16:29:25 +0530

    updated

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL ='mongodb+srv://root:root@books-store-mern.3ajih3n.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
